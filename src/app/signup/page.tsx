49eb7e6c30292ec9a172a69793becaad56ea7465
Cody <cshaner04@gmail.com>
2023-07-03 11:14:36 -0500

    .env to gitignore

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 export default function SignUpPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignUp = async () => {};
+
   return (
-    <div className="flex">
-      <h1 className="text-center text-white">Sign Up</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="">Sign Up</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input type="text" id="username" value={user.username} />
     </div>
   );
 }
