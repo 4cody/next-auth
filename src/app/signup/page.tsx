b4556e183231e7b5b29759ee6316e9a5719e712a
Cody <cshaner04@gmail.com>
2023-07-03 12:51:37 -0500

    user pages & login/signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -18,7 +18,39 @@ export default function SignUpPage() {
       <h1 className="">Sign Up</h1>
       <hr />
       <label htmlFor="username">username</label>
-      <input type="text" id="username" value={user.username} />
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="UserName"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onSignUp}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outlined-none focused:border-gray-600"
+      >
+        Signup Here
+      </button>
+      <Link href="/login">Visit Login</Link>
     </div>
   );
 }
